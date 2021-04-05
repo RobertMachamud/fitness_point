18f7672ad940c62204a1747bb2c1e1c3145a9cb6
Robert Machamud <robert_machamud@yahoo.com>
2021-04-05 12:40:56 +0000

    added filter by category (courses) functionality

--- offers/views.py
+++ offers/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, reverse, redirect, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Offer
+from .models import Offer, Category
 
 
 def all_offers(request):
@@ -9,6 +9,7 @@ def all_offers(request):
     """ A view to show all offers, including search queries and sorting """
 
     search_query = None
+    search_category = None
     offers = Offer.objects.all()
 
     if request.GET:
@@ -21,9 +22,16 @@ def all_offers(request):
             queries_to_search = Q(name__icontains=search_query) | Q(descr__icontains=search_query)
             offers = offers.filter(queries_to_search)
 
+        if 'category' in request.GET:
+            categories_to_search = request.GET['category'].split(',')
+            offers = offers.filter(category__name__in=categories_to_search)
+            categories_to_search = Category.objects.filter(
+                name__in=categories_to_search)
+
     content = {
         'offers': offers,
         'to_search': search_query,
+        'curr_category': categories_to_search,
     }
 
     return render(request, 'offers/offers.html', content)
