36f437dd1b1211634ac3e44f4afd31ba8ffba785
Robert Machamud <robert_machamud@yahoo.com>
2021-04-06 15:58:17 +0000

    updated product sizes fuctionality

--- cart/contexts.py
+++ cart/contexts.py
@@ -12,15 +12,27 @@ def cart_content(request):
     curr_taxes_prec = settings.TAXES_PERC
     cart = request.session.get('cart', {})
 
-    for item_id, qty in cart.items():
-        offer = get_object_or_404(Offer, pk=item_id)
-        cart_total += qty * offer.price
-        items_amt += qty
-        cart_items.append({
-            'item_id': item_id,
-            'qty': qty,
-            'offer': offer
-        })
+    for item_id, data_item in cart.items():
+        if isinstance(data_item, int):   
+            offer = get_object_or_404(Offer, pk=item_id)
+            cart_total += data_item * offer.price
+            items_amt += data_item
+            cart_items.append({
+                'item_id': item_id,
+                'qty': data_item,
+                'offer': offer
+            })
+        else:
+            offer = get_object_or_404(Offer, pk=item_id)
+            for sz, qty in data_item['items_by_sz'].items():
+                cart_total += qty * offer.price
+                items_amt += data_item
+                cart_items.append({
+                    'item_id': item_id,
+                    'qty': data_item,
+                    'offer': offer,
+                    'size': sz,
+                })
 
     if cart_total < settings.FREE_DEL:
         delivery = cart_total + Decimal(settings.DEL_COSTS)
