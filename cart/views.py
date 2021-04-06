fba80ff1ac68ae319bf505c98e07e92bdeeb514e
Robert Machamud <robert_machamud@yahoo.com>
2021-04-06 15:41:40 +0000

    updated cart views - option for sizes/multible sizes

--- cart/views.py
+++ cart/views.py
@@ -12,14 +12,28 @@ def add_item_to_cart(request, item_id):
 
     """ Adds a quantity of the selected item to the cart """
 
+    item_size = None
     qty = int(request.POST.get('qty'))
     cart = request.session.get('cart', {})
     curr_url = request.POST.get('curr_url')
 
-    if item_id in list(cart.keys()):
-        cart[item_id] += qty
-    else:
-        cart[item_id] = qty
+    if 'item_size' in request.POST:
+        item_size = request.POST['item_size']
+    cart = request.session.get('cart', {})
+
+    if item_size:
+        if item_id in list(cart.keys()):
+            if item_size in cart[item_id]['items_by_sz'].keys():
+                cart[item_id]['items_by_sz'][item_size] += qty
+            else:
+                cart[item_id]['items_by_sz'][item_size] = qty
+        else:
+            cart[item_id] = {'items_by_sz': {item_size: qty}}
+    elif not item_size:
+        if item_id in list(cart.keys()):
+            cart[item_id] += qty
+        else:
+            cart[item_id] = qty
 
     request.session['cart'] = cart
     return redirect(curr_url)
