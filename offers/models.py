037e395e7532b1b0eba5f5c631c22fc01b6d2a0c
Robert Machamud <robert_machamud@yahoo.com>
2021-04-06 13:40:45 +0000

    migrated new Offers model (added is_shoe and has_size)

--- offers/models.py
+++ offers/models.py
@@ -19,18 +19,20 @@ class Category(models.Model):
 class Offer(models.Model):
     category = models.ForeignKey(
         'Category', null=True, blank=True, on_delete=models.SET_NULL)
-    name = models.CharField(max_length=254)
     descr = models.TextField()
-    is_course = models.BooleanField(default=True, null=True, blank=True)
-    for_beginners = models.BooleanField(default=False, null=True, blank=True)
-    price = models.DecimalField(max_digits=6, decimal_places=2)    
+    name = models.CharField(max_length=254)
+    image = models.ImageField(null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)  
     day_one = models.CharField(max_length=254, null=True, blank=True)
     day_two = models.CharField(max_length=254, null=True, blank=True)
     day_span = models.CharField(max_length=254, null=True, blank=True)
     time_one = models.CharField(max_length=254, null=True, blank=True)
     time_two = models.CharField(max_length=254, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
+    is_shoe = models.BooleanField(default=False, null=True, blank=True)
+    is_course = models.BooleanField(default=True, null=True, blank=True)
+    has_sizes = models.BooleanField(default=False, null=True, blank=True)
+    for_beginners = models.BooleanField(default=False, null=True, blank=True)
 
     def __str__(self):
         return self.name
