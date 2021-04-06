833ebad701f468fa30bc0caa688c8ab2aa86b9db
Robert Machamud <robert_machamud@yahoo.com>
2021-04-06 10:07:45 +0000

    added functionality to add to cart & tested

--- cart/urls.py
+++ cart/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.to_cart, name='to_cart'),
+    path('add/<item_id>', views.add_item_to_cart, name='add_item_to_cart'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def to_cart(request):
@@ -6,3 +6,21 @@ def to_cart(request):
     """ A view that renders the cart page """
 
     return render(request, 'cart/cart.html')
+
+
+def add_item_to_cart(request, item_id):
+
+    """ Adds a quantity of the selected item to the cart """
+
+    qty = int(request.POST.get('qty'))
+    cart = request.session.get('cart', {})
+    curr_url = request.POST.get('curr_url')
+
+    if item_id in list(cart.keys()):
+        cart[item_id] += qty
+    else:
+        cart[item_id] = qty
+
+    request.session['cart'] = cart
+    print("BBBBBBBBBBBBBBBBBBBBBBBB", request.session['cart'])
+    return redirect(curr_url)
