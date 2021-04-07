d78245c386ef50cd5574ea558cb3b060cde595e7
Robert Machamud <robert_machamud@yahoo.com>
2021-04-07 21:58:34 +0000

    added views for secure checkout app

--- sec_checkout/views.py
+++ sec_checkout/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+from .forms import OrderForm
 
-# Create your views here.
+
+def sec_checkout(request):
+    cart = request.session.get('cart', {})
+    if not cart:
+        messages.error(request, "There's nothing in your cart")
+        return redirect(reverse('offers'))
+
+    order_form = OrderForm()
+    template = 'sec_checkout/sec_checkout.html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
