61306b6f0e4ad5c168c13004b606a2b5fe760462
Robert Machamud <robert_machamud@yahoo.com>
2021-04-08 12:15:07 +0000

    updated checkout js

--- sec_checkout/static/sec_checkout/js/stripe_elements.js
+++ sec_checkout/static/sec_checkout/js/stripe_elements.js
@@ -27,3 +27,50 @@ let style = {
 
 let card = elements.create('card', {style: style});
 card.mount('#card-element');
+
+// Handle realtime validation errors on the card element
+card.addEventListener('change', (e) => {
+    let errorDiv = document.getElementById('card-errors');
+    if (e.error) {
+        let html = `
+            <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+            </span>
+            <span>${e.error.message}</span>
+        `;
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    }
+});
+
+
+// Handle form submit
+let form = document.getElementById('payment-form');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    card.update({ 'disabled': true});
+    $('#submit-button').attr('disabled', true);
+    stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+            card: card,
+        }
+    }).then(function(result) {
+        if (result.error) {
+            let errorDiv = document.getElementById('card-errors');
+            let html = `
+                <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+                </span>
+                <span>${result.error.message}</span>`;
+            $(errorDiv).html(html);
+            card.update({ 'disabled': false});
+            $('#submit-button').attr('disabled', false);
+        } else {
+            if (result.paymentIntent.status === 'succeeded') {
+                form.submit();
+            }
+        }
+    });
+});
