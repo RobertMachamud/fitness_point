f51f023e0abefd85a163bccc1eb519dc1dc3ec4a
Robert Machamud <robert_machamud@yahoo.com>
2021-04-09 19:32:34 +0000

    updated allauth templates

--- user_profiles/views.py
+++ user_profiles/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import UserProfile
 
 
 def user_profile(request):
 
     """ Displays the user's profile """
 
+    user_profile = get_object_or_404(UserProfile, user=request.user)
+
     template = 'user_profiles/user_profile.html'
-    content = {}
+    content = {
+        'user_profile': user_profile,
+    }
 
     return render(request, template, content)
