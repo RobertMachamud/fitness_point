52fdba53711fcef64f88652f916f98c813a0cb38
Robert Machamud <robert_machamud@yahoo.com>
2021-04-10 12:44:23 +0000

    added profile forms, profile views, profile template, css and external js for country sel, small upd toast succ.

--- user_profiles/views.py
+++ user_profiles/views.py
@@ -1,16 +1,30 @@
+
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 from .models import UserProfile
+from .forms import UserProfileForm
 
 
 def user_profile(request):
 
-    """ Displays the user's profile """
+    """ Displays the user's profile. """
 
     user_profile = get_object_or_404(UserProfile, user=request.user)
 
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=user_profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully')
+
+    form = UserProfileForm(instance=profile)
+    orders = user_profile.orders.all()
+
     template = 'user_profiles/user_profile.html'
     content = {
-        'user_profile': user_profile,
+        'form': form,
+        'orders': orders,
+        'on_profile_page': True
     }
 
     return render(request, template, content)
